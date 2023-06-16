1ab34c0ab023f3170dae6671a791d5e39634dcff
Vishal Prasad <vishal.prasad@innoraft.com>
2023-06-16 12:04:09 +0530

    Defined the EnablerService class.

--- web/modules/custom/rsvplist/src/EnablerService.php
+++ web/modules/custom/rsvplist/src/EnablerService.php
@@ -4,3 +4,19 @@
  * @file
  * Conatinsthe EnablerService Class for the RSVP Module enabling service
  */
+
+namespace Drupal\rsvplist;
+
+use Drupal\core\database\Connection;
+
+class EnablerService {
+
+  protected $database_connection;
+
+  /**
+   * Establishing the Database Connection
+   */
+  public function __construct(Connection $connection) {
+    $this->database_connection = $connection;
+  }
+}
