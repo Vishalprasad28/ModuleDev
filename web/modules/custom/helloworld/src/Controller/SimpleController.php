f6b684bcc50b4d59ee6bf9c5192e0b8dbb90b99d
Vishal Prasad <vishal.prasad@innoraft.com>
2023-06-22 17:40:50 +0530

    FT2023-339 Added the Custom Permission and assigned it to a route.

---
+++ web/modules/custom/helloworld/src/Controller/SimpleController.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Contains the Controller code for the simple page
+ */
+namespace Drupal\helloworld\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class SimpleController extends ControllerBase {
+
+  /**
+   * @var object
+   */
+  private $current_user;
+
+  public function __construct() {
+    $this->current_user = \Drupal::currentUser();
+  }
+
+  /**
+   * @method
+   * To Return a simple markup.
+   * 
+   * @return array
+   */
+  public function helloUser() {
+    $content = [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hello <strong>@user</strong> how are you? <br><br> gocha we got your email id: @email',
+      [
+        '@user' => $this->current_user->getAccountName(),
+        '@email' => $this->current_user->getEmail(),
+      ]),
+    ];
+
+    return $content;
+  }
+}
