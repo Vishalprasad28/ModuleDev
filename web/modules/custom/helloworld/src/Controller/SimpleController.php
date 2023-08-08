5e496cfdcf69e0b398900545cc380600f4fea428
Vishal Prasad <vishal.prasad@innoraft.com>
2023-08-08 23:26:06 +0530

    FT2023:340 Modified the controller file of helloworld module.

--- web/modules/custom/helloworld/src/Controller/SimpleController.php
+++ web/modules/custom/helloworld/src/Controller/SimpleController.php
@@ -1,60 +1,64 @@
 <?php
 
-/**
- * @file
- * Contains the Controller code for the simple page
- */
 namespace Drupal\helloworld\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * A SimpleController class to handle the requests coming from different routes.
+ */
 class SimpleController extends ControllerBase {
 
   /**
-   * @var object
+   * Contains the Current Logged In User details.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
    */
-  private $current_user;
+  protected $currentUser;
 
-  public function __construct() {
-    $this->current_user = \Drupal::currentUser();
+  /**
+   * Constructs the Current User detailobject.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $user
+   *   Contains the AccountInterface Object, containing the current logged in
+   *   user details.
+   */
+  public function __construct(AccountInterface $user) {
+    $this->currentUser = $user;
   }
 
   /**
-   * @method
-   * To Return a simple markup.
-   * 
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Function to print sinple message on UI.
+   *
    * @return array
+   *   Returns the Render Array to display on the UI.
    */
   public function helloUser() {
     $content = [
       '#type' => 'markup',
       '#markup' => $this->t('Hello <strong>@user</strong> how are you? <br><br> gocha we got your email id: @email',
       [
-        '@user' => $this->current_user->getAccountName(),
-        '@email' => $this->current_user->getEmail(),
+        '@user' => $this->currentUser->getAccountName(),
+        '@email' => $this->currentUser->getEmail(),
       ]),
+      '#cache' => [
+        'contexts' => ['user'],
+        'tags' => ['user:' . $this->currentUser->id()],
+      ],
     ];
 
     return $content;
   }
 
-  /**
-   * @method
-   * To Return a simple markup.
-   * 
-   * @return array
-   */
-  public function dynamicGreeting(string $name, string $company) {
-    $content = [
-      '#type' => 'markup',
-      '#markup' => t('Hello <strong> @user </strong> You are an employee at <strong> @company </strong>',
-       [
-        '@user' => $name,
-        '@company' => $company,
-       ]
-       ),
-      ];
-
-    return $content;
-  }
 }
